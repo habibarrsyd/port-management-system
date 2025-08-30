496a8fb9952c85a92c040c7bdd39ed0e607fb165
habibarrsyd <arrsydhabib@apps.ipb.ac.id>
2025-08-30 10:42:30 +0700

    dashboard baru, transaksi dibenerin belum di tailwind in tapi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FileUpload from "./component/fileupload";
 import SuccessPage from "./pages/anotherhome";
-// import Nav from "./component/Frame/Navbar";
-// import Header from "./component/header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./component/Frame/Navbar";
 import TransactionsTable from "./pages/transaction";
-// import Dashboard from "./pages/dashboard";
-// import { StickyNavbar } from "./component/Frame/Navbar";
-// import { ComplexNavbar } from "./component/Frame/Navbar";
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import ComplexNavbar  from "./component/Frame/Navbar";
+import Dashboard from "./pages/dashboard";
+
 
 function App() {
   return (
@@ -25,7 +19,7 @@ function App() {
         <Route path="/success" element={<SuccessPage />} />
         <Route path="/upload" element={<FileUpload />} />
         <Route path="/transactions" element={<TransactionsTable />} />
-        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        <Route path="/dashboard" element={<Dashboard />} />
 
       </Routes>
       <ToastContainer position="top-right" autoClose={3000} />
@@ -34,27 +28,3 @@ function App() {
 }
 
 export default App;
-
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import FileUpload from "./component/fileupload";
-// import SuccessPage from "./pages/anotherhome";
-// import { StickyNavbar } from "./component/Frame/Navbar";
-// import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-
-// function App() {
-//   return (
-//     <Router>
-//       <StickyNavbar />
-//       <Routes>
-//         <Route path="/" element={<FileUpload />} />
-//         <Route path="/upload" element={<FileUpload />} />
-//         <Route path="/transactions" element={<SuccessPage />} />
-//       </Routes>
-//       <ToastContainer position="top-center" autoClose={3000} />
-//     </Router>
-//   );
-// }
-
-// export default App;
-
